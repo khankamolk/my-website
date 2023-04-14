4a809cdebefeb5f5400afc55c169bb51049e7fc0
unknown <khankamolk@berkeley.edu>
2023-04-14 04:36:43 -0700

    implemented toggle theme

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
+const body = document.body
+const btnTheme = document.querySelector('.fa-moon')
 
-const modeToggle = document.querySelector('#mode-toggle');
-const body = document.body;
-const modeKey = 'mode';
-
-// Check local storage for mode preference
-if (localStorage.getItem(modeKey) === 'dark') {
-  body.classList.add('dark-mode');
+const addThemeClass = (bodyClass, btnClass) => {
+  body.classList.add(bodyClass)
+  btnTheme.classList.add(btnClass)
 }
 
-// Toggle mode on button click
-modeToggle.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  const mode = body.classList.contains('dark-mode') ? 'dark' : 'light';
-  localStorage.setItem(modeKey, mode);
-});
+const getBodyTheme = localStorage.getItem('portfolio-theme')
+const getBtnTheme = localStorage.getItem('portfolio-btn-theme')
+
+addThemeClass(getBodyTheme, getBtnTheme)
+
+const isDark = () => body.classList.contains('dark')
+
+const setTheme = (bodyClass, btnClass) => {
+  console.log("Theme changed")
+  console.log(localStorage.getItem('portfolio-theme'))
+	body.classList.remove(localStorage.getItem('portfolio-theme'))
+	btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme'))
+
+  addThemeClass(bodyClass, btnClass)
+
+	localStorage.setItem('portfolio-theme', bodyClass)
+	localStorage.setItem('portfolio-btn-theme', btnClass)
+}
+
+const toggleTheme = () =>
+	isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun')
+
+btnTheme.addEventListener('click', toggleTheme)
+
+// Scroll 
+const scrollUp = () => {
+	const btnScrollTop = document.querySelector('.scroll-top')
+
+	if (
+		body.scrollTop > 500 ||
+		document.documentElement.scrollTop > 500
+	) {
+		btnScrollTop.style.display = 'block'
+	} else {
+		btnScrollTop.style.display = 'none'
+	}
+}
+
+document.addEventListener('scroll', scrollUp)
